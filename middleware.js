b2256c4635ce05f306515affbed64ce7a83e743d
Andreizabo <45977666+Andreizabo@users.noreply.github.com>
2020-06-07 00:14:59 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
 
 function changeResponseHeader(req, res, next) {
-	res.header("X-Powered-By", "get-a-life");
+	res.header("X-Powered-By", "no");
 	next();
 }
 
 function denyDotDot(req, res, next) {
-	if(req.url.indexOf("..") > -1) res.status(400).send("Epstein didn't kill himself");
+	if(req.url.indexOf("..") > -1) res.status(400).send("400-no");
 	else next();
 }
 
